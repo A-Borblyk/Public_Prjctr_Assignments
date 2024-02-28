762735590880d8146e9f7ba914f44f091a3100c6
a.borblyk <borblyk@gmail.com>
2024-02-28 19:30:06 +0100

    Add multiply functionality

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ def subtract_two_numbers(x, y):
     return x - y
 
 
+def multiply_two_numbers(x, y):
+    return x * y
+
+
 print(add_two_numbers(3, 5))
 
 print(subtract_two_numbers(3, 5))
+
+print(multiply_two_numbers(3, 5))
